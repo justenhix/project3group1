146f488decf1dde369328defa5eafce59481097f
Justen Hix <152797238+justenhix@users.noreply.github.com>
2024-04-08 18:07:27 -0400

    Add files via upload

---
+++ Project3Group1.sql
@@ -0,0 +1,43 @@
+drop table raleigh
+
+create table raleigh (
+year varchar not null,
+month varchar not null,
+value varchar not null,
+rate varchar not null
+
+);
+
+select * from raleigh
+
+drop table greensboro
+create table greensboro (
+year varchar not null,
+month varchar not null,
+value varchar not null,
+rate varchar not null
+
+);
+
+select * from greensboro
+
+drop table winston
+create table winston (
+year varchar not null,
+month varchar not null,
+value varchar not null,
+rate varchar not null
+);
+
+select * from winston
+
+drop table charlotte
+create table charlotte (
+year varchar not null,
+month varchar not null,
+value varchar not null,
+rate varchar not null
+
+);
+
+select * from charlotte
