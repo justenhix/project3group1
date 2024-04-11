7d53bb99b4301f400ba8f02500ef48405a553e2a
Ashleigh <152832328+Ashleigh-Wittenberg@users.noreply.github.com>
2024-04-11 18:34:44 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # Import the dependencies.
 from flask import Flask, jsonify
-import csv
 import os
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import sqlite3
+import pandas as pd
 
 
 #################################################
@@ -26,12 +25,41 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return (
-        f"Available Routes:<br/> Each link shows the month, year, unemployment rate, and number of unemployeed individuals for the respective city.<br/><br/>"
+        f"Available Routes:<br/> The first link shows the unemployment rate, and number of unemployed individuals for the respective city based on the year and month. <br/><br/>"
+        f"/api/v1.0/year_month_data<br/><br/>"
+        f"Each link below shows the month, year, unemployment rate, and number of unemployed individuals for the respective city.<br/><br/>"
         f"/api/v1.0/raleigh_unep_data<br/><br/>"
         f"/api/v1.0/charlotte_unep_data<br/><br/>"
         f"/api/v1.0/greensboro_unep_data<br/><br/>"
         f"/api/v1.0/winstonsalem_unep_data"
     )
+
+
+
+@app.route("/api/v1.0/year_month_data")
+def year_month_sqlite_to_json():
+    # SQLite database file path
+    sqlite_file_path = os.path.join(os.path.dirname(__file__), 'Output', 'year_month.sqlite')
+
+    # Check if the file exists
+    if not os.path.isfile(sqlite_file_path):
+        return jsonify({"error": "SQLite file not found"}), 404
+    
+    # Connect to SQLite database
+    conn = sqlite3.connect(sqlite_file_path)
+    cursor = conn.cursor()
+
+    # Fetch data from SQLite table
+    cursor.execute("SELECT * FROM year_month")
+    data = cursor.fetchall()
+
+    # Close connection
+    conn.close()
+
+    # Convert SQLite data to JSON
+    json_data = jsonify(data)
+
+    return json_data
 
 
 
